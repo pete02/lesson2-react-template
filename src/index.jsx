85ae8371fbf3a8782f1d99be818449c4dde342c2
Petrik Räsänen <petrikrasanen@Petrik-MacBook-Air.local>
2019-02-13 16:09:50 +0200

    Do homework

--- src/index.jsx
+++ src/index.jsx
@@ -7,17 +7,35 @@ import './assets/stylesheets/style.css'
 
 /****** ADD YOUR CODE AFTER THIS LINE ******/
 
-const Hello = () => (
-  <div>Hello World!</div>
-);
 
 class App extends Component {
-  render() {
-    return (
-      <Hello />
-    );
+    constructor(props) {
+      super(props);
+      this.state = {
+        data: "asdf"
+      }
+    }
+  
+    componentDidMount() {
+      console.log(this);
+      fetch('https://weatherapp.eficode.fi/api/forecast')
+      .then(response => {
+        return response.json();
+      })
+      .then(myJson => {
+        this.setState({
+          data: JSON.stringify(myJson)
+        });
+      });
+    }
+  
+    render() {
+      return (
+        this.state.data
+      );
+    }
   }
-}
+  
 
 /****** DO NOT DELETE AFTER THIS LINE ******/
 
